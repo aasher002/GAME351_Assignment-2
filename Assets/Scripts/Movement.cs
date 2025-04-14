dd954e7938103bccd0a5f299b6dc57b41e5a67cb
RotagDev <67494630+RotagDev@users.noreply.github.com>
2025-04-13 21:19:24 -0700

    Player Movement & Camera
    + vehicle control with WASD
    + camera following vehicle
       + camera stays above terrain

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -2,30 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Movement : MonoBehaviour
-{
+public class Movement : MonoBehaviour {
+    public float moveSpeed = 30f;
+    public float turnSpeed = 60f;
+
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
+        
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        // Hint: The global static variable "Terrain.activeTerrain" 
-        // may be helpful or have useful methods for user here or in
-        // other scripts.
+    void Update() {
         Terrain terrain = Terrain.activeTerrain;
 
         Vector3 position = transform.position;
-        
+
         // set the game object's translation (not an increment)
         transform.position = position;
 
-        // translate by 0.1m on Z axis each frame for as long as
-        // the space bar is held down
-        if (Input.GetKey (KeyCode.Space))
+
+        if (Input.GetKey(KeyCode.W)) {
             // increment the game object's translation
-            transform.Translate(0, 0, 0.1f);
+            transform.Translate(new Vector3(0, 0, moveSpeed * Time.deltaTime));
+        }
+        else if (Input.GetKey(KeyCode.S)) {
+            transform.Translate(new Vector3(0, 0, -moveSpeed * Time.deltaTime));
+        }
+        
+        if (Input.GetKey(KeyCode.A)) {
+            transform.Rotate(0, -turnSpeed * Time.deltaTime, 0);
+        }
+        else if (Input.GetKey(KeyCode.D)) {
+            transform.Rotate(0, turnSpeed * Time.deltaTime, 0);
+        }
     }
 }
